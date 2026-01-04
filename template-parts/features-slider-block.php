75facf2292bf834195f203b8c663485946a26e1e
engelgart-oleg <olegengelgart@gmail.com>
2026-01-04 20:54:38 +0500

    fix: modal dialog

---
+++ template-parts/features-slider-block.php
@@ -0,0 +1,69 @@
+<div class="container features-container">
+
+  <!-- Slider main container -->
+  <div class="swiper features-slider">
+    <!-- Additional required wrapper -->
+    <ul class="swiper-wrapper header-features">
+      <!-- Slides -->
+      <li class="swiper-slide header-features-item">
+        <svg width="36" height="36">
+          <use href="img/sprite.svg#time"></use>
+        </svg>
+        <p class="header-features-text">
+          Непрерывная работа c 2017 года
+        </p>
+      </li>
+      <li class="swiper-slide header-features-item">
+        <svg width="36" height="36">
+          <use href="img/sprite.svg#sertificate"></use>
+        </svg>
+        <p class="header-features-text">
+          Вся продукция сертифицирована
+        </p>
+      </li>
+      <li class="swiper-slide header-features-item">
+        <svg width="36" height="36">
+          <use href="img/sprite.svg#control"></use>
+        </svg>
+        <p class="header-features-text">
+          Контроль качества на всех этапах
+        </p>
+      </li>
+      <li class="swiper-slide header-features-item">
+        <svg width="36" height="36">
+          <use href="img/sprite.svg#delivery"></use>
+        </svg>
+        <p class="header-features-text">
+          Возможны поставки по всей России
+        </p>
+      </li>
+      <li class="swiper-slide header-features-item">
+        <svg width="36" height="36">
+          <use href="img/sprite.svg#speed"></use>
+        </svg>
+        <p class="header-features-text">
+          Оперативное производство
+        </p>
+      </li>
+
+    </ul>
+    <!-- /.Additional required wrapper -->
+
+    <!-- If we need navigation buttons -->
+    <div class="slider-buttons">
+      <div class="slider-button-prev">
+        <svg width="36" height="24">
+          <use href="img/sprite.svg#arrow-prev"></use>
+        </svg>
+      </div>
+      <div class="slider-button-next">
+        <svg width="36" height="24">
+          <use href="img/sprite.svg#arrow-next"></use>
+        </svg>
+      </div>
+    </div>
+
+  </div>
+  <!-- /.Slider main container -->
+
+</div>
